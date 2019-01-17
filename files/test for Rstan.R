0cbacea9d6356e2a268b36b89338033effe0d92a
TAKESHI <abelardccwang@gmail.com>
2019-01-17 13:15:41 +0900

    rebuilding site 2019年  1月 17日 木曜日 13:15:41 JST

--- files/test for Rstan.R
+++ files/test for Rstan.R
@@ -5,3 +5,7 @@ posterior2 <- rstan::extract(fit, inc_warmup = TRUE, permuted = FALSE)
 p <- mcmc_trace(posterior2, n_warmup = 0,
                 facet_args = list(nrow = 2, labeller = label_parsed))
 p
+
+
+p <- mcmc_acf_bar(posterior2)
+p
