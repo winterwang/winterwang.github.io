995052f0f784480b4c7d202020754d4780b79b66
winterwang <chaochen.wang@bayer.com>
2025-10-22 23:02:37 +0900

    update today's recording

--- scripts/Generate new post.r
+++ scripts/Generate new post.r
@@ -1,10 +1,10 @@
 library(blogdown)
 
 blogdown::new_post(
-  title  = "2025年10月20日 星期一",
+  title  = "2025年10月22日 星期三", # change to today
   ext    = ".md",
   subdir = "post",
-  slug   = "2025-10-20",   # 建議加英文 slug，避免網址含中文
+  slug   = "2025-10-22",   # 建議加英文 slug，避免網址含中文
   categories = c("Life"),
-  tags       = c("日常","記錄")
+  tags       = c("日常","記錄","work")
 )
